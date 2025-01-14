6ec217cd7d714fb8b39da629f4ca51b3b85b77f3
Niklas Mollenhauer <nikeee@outlook.com>
2025-01-14 16:59:34 +0100

    Add webnfc types

---
+++ src/WebNFC.js
@@ -0,0 +1,56 @@
+// @ts-check
+
+// Loosely based on the WebNFC standard:
+// https://github.com/w3c/web-nfc/blob/gh-pages/web-nfc.d.ts
+// https://w3c.github.io/web-nfc/#parsing-ndef-records
+
+/** @typedef {NDEFRecord["recordType"]} NDEFRecordType */
+
+/**
+ * @typedef {{
+ *  id: string | null;
+ *  lang: null | string;
+ * }} NDEFRecordBase
+ *
+ * @typedef {{
+ *   recordType: "empty";
+ *   id: null;
+ *   lang: null;
+ *   encoding: null;
+ *   mediaType: null;
+ *   data: null;
+ * }} EmptyNDEFRecord
+ *
+ * @typedef {{
+ *  recordType: "text";
+ *  mediaType: null;
+ *  data: null | DataView;
+ *  lang: string;
+ * }} TextNDEFRecord
+ *
+ * @typedef {{
+ *  recordType: "url";
+ *  mediaType: null;
+ *  data: null | DataView;
+ *  lang: null;
+ *  encoding: null;
+ * }} UrlNDEFRecord
+ *
+ * @typedef {{
+ *  recordType: "smart-poster";
+ *  mediaType: null;
+ *  data: null | DataView;
+ * }} SmartPosterNDEFRecord
+ *
+ * @typedef {{
+ *  recordType: "mime";
+ *  mediaType: string;
+ * }} MimeNDEFRecord
+ *
+ * @typedef {{
+ *  recordType: "unknown";
+ *  mediaType: null;
+ * }} UnknownNDEFRecord
+ *
+ * @typedef {EmptyNDEFRecord | TextNDEFRecord | UrlNDEFRecord | SmartPosterNDEFRecord | MimeNDEFRecord | UnknownNDEFRecord} NDEFRecord
+ */
